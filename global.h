2a841e657daabfee0090ca4ab766680bb201750b
Thiru-543 <sthiruselva11@gmail.com>
2025-10-22 19:12:40 +0530

    modified global.h file

---
+++ global.h
@@ -0,0 +1,17 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+char na[20];
+char na1[20];
+char mo[20];
+char mo1[20];
+char pi[10];
+char pass[10];
+char truser[20];
+char truser1[20];
+char truser2[20];
+char acc1[20];
+char acc2[20];
+char na0[20];
+
+#endif
